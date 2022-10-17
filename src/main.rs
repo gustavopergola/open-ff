ea0321fa54905f3e609460d55b53d37a99f761e0
yago.silva <yagobatistasilva@gmail.com>
2022-10-17 19:51:44 -0300

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+#[get("/ping")]
+fn ping() -> &'static str {
+    "pong :)"
+}
+
+fn main() {
+    rocket::ignite().mount("/health", routes![ping]).launch();
+}
